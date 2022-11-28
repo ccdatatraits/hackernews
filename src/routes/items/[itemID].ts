8636be23dca7b260f17b17044f968898b57c5bb6
Sohail M. Khan <sokhan@mediamath.com>
2022-11-28 08:03:36 +0800

    API Endpoint for Items

---
+++ src/routes/items/[itemID].ts
@@ -0,0 +1,8 @@
+import { APIEvent, json } from "solid-start";
+
+export async function GET({ params }: APIEvent) {
+  // console.log(`ITEM#: ${params.itemID}`);
+  return json({
+    "ID": params.itemID
+  })
+}
