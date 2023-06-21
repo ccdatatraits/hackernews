2f3906b46dffb7e74e9b336e654537c498295f08
Sohail M. Khan <sokhan@mediamath.com>
2023-06-22 06:31:41 +0800

    Adding Bonus

--- src/routes/items/[itemID].ts
+++ src/routes/items/[itemID].ts
@@ -3,6 +3,7 @@ import { APIEvent, json } from "solid-start";
 export async function GET({ params }: APIEvent) {
   // console.log(`ITEM#: ${params.itemID}`);
   return json({
-    "ID": params.itemID
+    "ID": params.itemID,
+    "Bonus": true,
   })
 }
